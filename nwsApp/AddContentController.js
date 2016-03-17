d2b09b5187d64eb05fa27a6eb76517632fa631a8
Calvin Miracle <cbmira01@gmail.com>
2016-03-17 19:24:16 -0400

    Adding ads now works.

--- nwsApp/AddContentController.js
+++ nwsApp/AddContentController.js
@@ -1,4 +1,31 @@
 
-  nwsApp.controller("AddContentController", ["$scope", function($scope) {
-    $scope.greeting = "Hello!";
-  }]);
+  nwsApp.controller("AddContentController", ["$scope", "$http", "$sce", "myServices", 
+    function ($scope, $http) {
+      $scope.errors = [];
+      $scope.msgs = [];
+
+      $scope.createAd = function() {
+        $scope.errors.splice(0, $scope.errors.length);
+        $scope.msgs.splice(0, $scope.msgs.length);
+            
+        var createJson = {
+          "link": $scope.adLink 
+        };
+        
+        $http.post("http://localhost/nws/php/createAd.php", createJson)
+          .success(function(data, status, headers, config) {
+            if (data.msg !== "") {
+                $scope.msgs.push(data.msg);
+            } else {
+                $scope.errors.push(data.error);
+            }
+          })
+          .error(function(data, status) {
+            $scope.errors.push(status);
+            alert("Error caught in nwsApp.js.AddContentController: " + status);
+          });
+      };
+
+// ----------------
+    }
+  ]);
