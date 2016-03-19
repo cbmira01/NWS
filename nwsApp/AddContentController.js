89ddd158ae1fccb3b095e1fdbca285642f019cd1
Calvin Miracle <cbmira01@gmail.com>
2016-03-19 14:12:48 -0400

    Feature update now works.

--- nwsApp/AddContentController.js
+++ nwsApp/AddContentController.js
@@ -1,24 +1,24 @@
 
   nwsApp.controller("AddContentController", ["$scope", "$http", "$route", "$sce", "myServices", 
-    function ($scope, $http, $route) {
+    function ($scope, $http, $route, $sce, myServices) {
 
-//update feature
       $scope.updateFeature = function() {
         var json = {
-          "id":" 1,
+          "id": 1,
           "title": $scope.featureTitle, 
           "article": $scope.featureArticle,
           "image1": $scope.featureImage1,
           "image2": $scope.featureImage2,
           "image3": $scope.featureImage3
         };
-        $http.post("http://localhost/nws/php/updateFeature.php, json)
+
+        $http.post("http://localhost/nws/php/updateFeature.php", json)
           .success(function(data, status, headers, config) {
           })
           .error(function(data, status) {
             alert("Error caught in nwsApp.js.AddContentController: " + status);
-          });
-      };
+          }); 
+      }; 
 
 //article
 //video
