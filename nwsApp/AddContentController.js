36a9d3a2da2e5452977a4601253eac8e3f31b9a2
Calvin Miracle <cbmira01@gmail.com>
2016-03-19 16:53:00 -0400

    AddContentController is now blocked out.

--- nwsApp/AddContentController.js
+++ nwsApp/AddContentController.js
@@ -14,23 +14,66 @@
 
         $http.post("http://localhost/nws/php/updateFeature.php", json)
           .success(function(data, status, headers, config) {
-          })
+        })
           .error(function(data, status) {
             alert("Error caught in nwsApp.js.AddContentController: " + status);
-          }); 
+        }); 
       }; 
 
-//article
-//video
+      $scope.createArticle = function() {
+            //   -- call getuuid function here --
+        var json = {
+          "uuid":,
+          "hfso":,
+          "title":,
+          "image":,
+          "text":
+        };
+        $scope.createContent("articles", json);
+      };
+
+/*       $scope.createVideo = function() {
+        var json = {
+          "site":,
+          "type":,
+          "title":,
+          "description":,
+          "link":
+        };
+        $scope.createContent("videos", json);
+      }; */
+
       $scope.createAd = function() {
         var json = {
           "link": $scope.adLink 
         };
         $scope.createContent("ads", json);
       };
-//archive
-//service
-//officer
+
+/*       $scope.createArchive = function() {
+        var json = {
+          "title":,
+          "link":
+        };
+        $scope.createContent("archives", json);
+      }; */
+
+/*       $scope.createService = function() {
+        var json = {
+          "text":
+        };
+        $scope.createContent("services", json);
+      }; */
+
+/*       $scope.createOfficer = function() {
+        var json = {
+          "name":,
+          "email":,
+          "description":,
+          "photo":
+        };
+        $scope.createContent("officers", json);
+      }; */
 
       $scope.createContent = function(table, json) {
         $http.post("http://localhost/nws/php/create.php?table=" + table, json)
